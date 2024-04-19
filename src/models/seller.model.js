5723f0c2d2c55349a7225c138ee847786b5a47a0
Amro Mukhtar <amromuktar@gmail.com>
2024-04-19 14:45:34 +0400

    add seller model

---
+++ src/models/seller.model.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import toJSON from './plugins/index';
+
+const imageSchema = mongoose.Schema({
+  id: {
+    type: 'string',
+    required: true
+  },
+  name: {
+    type: 'string',
+    required: true
+  },
+  img: {
+    type: 'string',
+    required: true
+  }
+});
+
+const sellerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required:true,
+    },
+    logo: {
+      type: imageSchema,
+      required: true,
+    },
+    imgList: {
+      type: [imageSchema],
+      required: true,
+      default: []
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true,
+      default: 'showroom'
+    },
+    verified: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+sellerSchema.plugin(toJSON);
+
+const Seller = mongoose.model('Seller', sellerSchema);
+
+export default Seller;
